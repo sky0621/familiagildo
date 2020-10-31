7b906b70b942a457d43ea2394b1c5ab09267976c
sky0621 <sky14work@gmail.com>
2020-11-01 01:06:06 +0900

    front

---
+++ src/frontend/settings/vuetify.ts
@@ -0,0 +1,13 @@
+export default function () {
+  return {
+    theme: {
+      themes: {
+        light: {
+          primary: '#C6FF00',
+          secondary: '#F0F4C3',
+          accent: '#FF6D00',
+        },
+      },
+    },
+  }
+}
