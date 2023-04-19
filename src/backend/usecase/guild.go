2a9252cd5c7209fc45cfb6052fba1d625ebae5aa
sky0621 <sky14work@gmail.com>
2023-04-20 01:29:05 +0900

    -

--- src/backend/usecase/guild.go
+++ src/backend/usecase/guild.go
@@ -1,1 +1,16 @@
 package usecase
+
+type Guild interface {
+	RequestCreateGuildByGuest()
+}
+
+func NewGuild() Guild {
+	return &guild{}
+}
+
+type guild struct {
+}
+
+func (g *guild) RequestCreateGuildByGuest() {
+
+}
