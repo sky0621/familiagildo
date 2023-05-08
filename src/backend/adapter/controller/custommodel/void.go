92794d476076010124591e43f7b9d2f673cfd5d4
sky0621 <sky14work@gmail.com>
2023-05-08 22:05:01 +0900

    -

--- src/backend/adapter/controller/custommodel/void.go
+++ src/backend/adapter/controller/custommodel/void.go
@@ -1,6 +1,7 @@
 package custommodel
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -8,8 +9,13 @@ type Void struct {
 }
 
 func (v *Void) MarshalGQL(w io.Writer) {
+	_, err := io.WriteString(w, "{}")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func (v *Void) UnmarshalGQL(a any) error {
+	fmt.Println("UnmarshalGQL")
 	return nil
 }
