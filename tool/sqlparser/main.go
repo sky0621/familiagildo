b3f20cf0b97714bf6d80a4acec6a76bfae5271e7
sky0621 <sky14work@gmail.com>
2023-05-12 09:42:00 +0900

    -

--- tool/sqlparser/main.go
+++ tool/sqlparser/main.go
@@ -6,8 +6,25 @@ import (
 	pg_query "github.com/pganalyze/pg_query_go/v4"
 )
 
+const selectSQL01 = `
+SELECT p.p_name, SUM(o.quantity), SUM(p.price * o.quantity)
+FROM order_desc AS o RIGHT JOIN product AS p
+ON p.p_id = o.p_id
+GROUP BY p.p_id, p.p_name ORDER BY SUM(p.price * o.quantity) DESC;
+`
+
+const insertSQL01 = `
+INSERT INTO guild (name, status) VALUES ($1, 1)
+RETURNING *;
+`
+
+const updateSQL01 = `
+UPDATE guild SET status = 2 WHERE id = $1
+RETURNING *;
+`
+
 func main() {
-	result, err := pg_query.Parse("SELECT * FROM guild WHERE id = $1;")
+	result, err := pg_query.Parse(selectSQL01)
 	if err != nil {
 		panic(err)
 	}
@@ -27,6 +44,20 @@ func main() {
 						if ok {
 							fmt.Println(nv.RangeVar.Relname)
 						}
+					}
+				}
+				is := s.GetInsertStmt()
+				if is != nil {
+					rel := is.GetRelation()
+					if rel != nil {
+						fmt.Println(rel.GetRelname())
+					}
+				}
+				us := s.GetUpdateStmt()
+				if us != nil {
+					rel := us.GetRelation()
+					if rel != nil {
+						fmt.Println(rel.GetRelname())
 					}
 				}
 			}
