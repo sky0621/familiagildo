df07e58c06aeae57385fd8f2ecdb0f734da8f428
sky0621 <sky14work@gmail.com>
2023-05-18 00:31:11 +0900

    -

--- tool/sqlparser/cmd/main.go
+++ tool/sqlparser/cmd/main.go
@@ -1,28 +1,74 @@
 package main
 
 import (
-	"embed"
+	"bufio"
 	"fmt"
-	"html/template"
 	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/sky0621/familiagildo/tool/sqlparser"
 )
-
-//go:embed testdata/*
-var testdata embed.FS
 
 func main() {
 	execMain()
 }
 
 func execMain() {
-	fsys, err := fs.Sub(testdata, "testdata")
-	if err != nil {
+	if err := filepath.WalkDir(filepath.Join("cmd", "testdata"), func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		fmt.Println(path)
+		fl, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+
+		sqlName := ""
+		sql := strings.Builder{}
+
+		sc := bufio.NewScanner(fl)
+		for sc.Scan() {
+			line := sc.Text()
+			if isBlankLine(line) {
+				fmt.Println("is blank line...")
+				continue
+			}
+			if isSQLNameLine(line) {
+				sqlName = getSQLName(line)
+				fmt.Println(sqlName)
+				continue
+			}
+			sql.WriteString(line + " ")
+
+		}
+		return nil
+	}); err != nil {
 		panic(err)
 	}
-	tpl := template.Must(template.ParseFS(fsys))
-	fmt.Println(tpl)
+}
+
+func isBlankLine(line string) bool {
+	return len(strings.Trim(line, " ")) == 0
+}
+
+func isSQLNameLine(line string) bool {
+	fmt.Println(line)
+	return strings.HasPrefix(strings.Trim(line, " "), "--name")
+}
+
+func getSQLName(line string) string {
+	// 形式　-- name: CreateGuestToken :one
+	tLine := strings.Trim(line, " ")
+	tpLine := strings.TrimPrefix(tLine, "--")
+	tokens := strings.Split(tpLine, ":")
+	if len(tokens) != 3 {
+		return ""
+	}
+	return tokens[1]
 }
 
 func execTest() {
